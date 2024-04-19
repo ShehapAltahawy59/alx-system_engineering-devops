cb968815a6b28efa7e0c9895e855cec0fb9001ff
shehap62 <shehapkhalil62@gmail.com>
2024-04-19 23:40:19 +0200

    edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,8 @@
+# Install puppet-lint
 package { 'flask':
   ensure     => '2.1.0',
   provider   => 'pip3',
   require    => Package['python3-pip'],
-}
-
-package { 'python3-pip':
-  ensure   => installed,
 }
 
 package { 'Werkzeug':
