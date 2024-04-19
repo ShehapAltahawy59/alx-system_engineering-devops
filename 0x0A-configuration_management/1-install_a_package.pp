e620c0f533f1304c59bdd40edddbbed5a6dc5938
shehap62 <shehapkhalil62@gmail.com>
2024-04-19 23:36:29 +0200

    edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
-#!/usr/bin/pup
-# install flask from pip3.
-exec { 'install-werkzeug':
-    command => '/usr/bin/pip3 install Werkzeug==2.0.0',
-    require => Package['python3-pip'],
-  }
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
