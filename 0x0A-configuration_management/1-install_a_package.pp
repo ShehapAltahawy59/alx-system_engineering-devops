9470a14a0ca7ed4a44de82058100f133bac7faec
shehap62 <shehapkhalil62@gmail.com>
2024-04-19 23:35:33 +0200

    edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 #!/usr/bin/pup
 # install flask from pip3.
+exec { 'install-werkzeug':
+    command => '/usr/bin/pip3 install Werkzeug==2.0.0',
+    require => Package['python3-pip'],
+  }
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
