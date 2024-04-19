e2a52cb2e93ef2db8657d1d6affa437395062329
shehap62 <shehapkhalil62@gmail.com>
2024-04-19 23:22:37 +0200

    edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-#!/usr/bin/pup
-# install flask from pip3.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+class flask {
+  package { 'python3-pip':
+    ensure => installed,
+  }
+
+  exec { 'install-flask':
+    command => '/usr/bin/pip3 install flask',
+    require => Package['python3-pip'],
+  }
 }
+
+include flask
