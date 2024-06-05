a1476d36382a655bd33c9c37e54ad26a6c4c12cb
shehap62 <shehapkhalil62@gmail.com>
2024-06-05 15:06:53 +0300

    edit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,16 +1,6 @@
+# Solve issue from wp-settings 
 
-# Fixes bad `phpp` extensions to `php`
-node 'default' {
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => "<title>Holberton – Just another WordPress site</title>
-<link rel=\"alternate\" type=\"application/rss+xml\" title=\"Holberton » Feed\" href=\"http://127.0.0.1/?feed=rss2\" />
-<link rel=\"alternate\" type=\"application/rss+xml\" title=\"Holberton » Comments Feed\" href=\"http://127.0.0.1/?feed=comments-rss2\" />
-<div id=\"wp-custom-header\" class=\"wp-custom-header\"><img src=\"http://127.0.0.1/wp-content/themes/twentyseventeen/assets/images/header.jpg\" width=\"2000\" height=\"1200\" alt=\"Holberton\" /></div>
-<h1 class=\"site-title\"><a href=\"http://127.0.0.1/\" rel=\"home\">Holberton</a></h1>
-<p>Yet another bug by a Holberton student</p>",
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0644',
-  }
+exec { 'fix':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
