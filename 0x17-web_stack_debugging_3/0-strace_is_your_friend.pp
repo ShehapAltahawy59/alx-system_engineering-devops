0a1796e0fb867e50653741e612d4562155b827e6
shehap62 <shehapkhalil62@gmail.com>
2024-06-05 15:03:41 +0300

    edit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,18 +1,19 @@
 
 # Fixes bad `phpp` extensions to `php`
-
-node 'default' {
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => "<title>Holberton – Just another WordPress site</title>
-<link rel=\"alternate\" type=\"application/rss+xml\" title=\"Holberton » Feed\" href=\"http://127.0.0.1/?feed=rss2\" />
-<link rel=\"alternate\" type=\"application/rss+xml\" title=\"Holberton » Comments Feed\" href=\"http://127.0.0.1/?feed=comments-rss2\" />
-<div id=\"wp-custom-header\" class=\"wp-custom-header\"><img src=\"http://127.0.0.1/wp-content/themes/twentyseventeen/assets/images/header.jpg\" width=\"2000\" height=\"1200\" alt=\"Holberton\" /></div>
-<h1 class=\"site-title\"><a href=\"http://127.0.0.1/\" rel=\"home\">Holberton</a></h1>
-<p>Yet another bug by a Holberton student</p>",
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0644',
-  }
+class { 'apache':
+  default_vhost => false,
 }
 
+apache::vhost { 'my_site':
+  port    => '80',
+  docroot => '/var/www/html',
+  directories => [
+    { 
+      path            => '/var/www/html',
+      options         => ['Indexes','FollowSymLinks','MultiViews'],
+      allow_override  => ['All'],
+      require         => ['all granted'],
+      directoryindex  => 'index.php',
+    },
+  ],
+}
